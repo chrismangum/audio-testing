096773a4743c7695b1f4c5dca04c62bbcb55303f
Chris Mangum <mtdbass5@gmail.com>
2014-05-01 16:33:09 -0400

    got metadata parsing working on the backend

--- public/js/app.coffee
+++ public/js/app.coffee
@@ -5,6 +5,7 @@ player = AV.Player.fromURL 'target/12 - Masser.mp3'
 player.on 'error', (e) ->
   throw e
 window.player = player
+player.preload()
 player.on 'metadata', (data) ->
   console.log data
   if data.coverArt
--- server/scanDir.js
+++ server/scanDir.js
@@ -1,11 +1,22 @@
 var fs = require('fs'),
-  _ = require('lodash');
+  _ = require('lodash'),
+  path = require('path');
+
+require('../public/js/aurora.js');
+require('../public/js/flac.js');
+require('../public/js/mp3.js');
+require('../public/js/aac.js');
 
 var ftypes = {
   '40': 'directory',
   '12': 'symlink',
   '10': 'file'
 };
+var validExtensions = [
+  '.m4a',
+  '.flac',
+  '.mp3'
+];
 
 var ignored = ['node_modules', 'vendor', '.git'];
 
@@ -13,14 +24,33 @@ function getFtype(mode) {
   return ftypes[mode.toString(8).slice(0, 2)];
 }
 
-function scanDir(path) {
+function logMetaData(path) {
+  fs.readFile(path, function (err, data) {
+    var player;
+    if (err) throw err;
+    player = AV.Player.fromBuffer(new Uint8Array(data));
+    player.preload();
+    player.on('metadata', function (data) {
+      console.log('Metadata for: ' + path);
+      console.log(_.pick(data, ['title', 'artist', 'album', 'genre']));
+    });
+  });
+}
+
+function hasValidExt(fileName) {
+  return validExtensions.indexOf(path.extname(fileName)) !== -1;
+}
+
+function scanDir(filePath) {
   var items = {};
-  _.each(fs.readdirSync(path), function (item) {
+  _.each(fs.readdirSync(filePath), function (item, i) {
     if (ignored.indexOf(item) === -1) {
-      var stat = fs.statSync(path + item);
+      var stat = fs.statSync(filePath + item);
       stat.type = getFtype(stat.mode);
       if (stat.type === 'directory') {
-        stat.children = scanDir(path + item + '/');
+        stat.children = scanDir(filePath + item + '/');
+      } else if (hasValidExt(item)) {
+        logMetaData(filePath + item);
       }
       items[item] = _.pick(stat, 'type', 'children');
     }
