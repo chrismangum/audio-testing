ad8f61227ed2c0e8c4424b774b6560b8593b3978
Chris Mangum <mtdbass5@gmail.com>
2014-05-07 12:55:06 -0400

    sped up metadata reading by only snanning the first 10,000 bytes

--- server/getTrackMetaData.js
+++ server/getTrackMetaData.js
@@ -9,19 +9,24 @@ require('../public/js/flac.js');
 require('../public/js/mp3.js');
 require('../public/js/aac.js');
 
-function getTrackMetaData(track, callback) {
-  fs.readFile(track, function (err, data) {
-    if (err) throw err;
+function processData(data, callback) {
     var player = AV.Player.fromBuffer(new Uint8Array(data));
     player.preload();
     player.on('metadata', function (data) {
-      callback(err, _.pick(data, [
+      callback(null, _.pick(data, [
         'title',
         'artist',
         'album',
         'genre'
       ]));
     });
+}
+var processDataOnce = _.once(processData);
+
+function getTrackMetaData(track, callback) {
+  var stream = fs.createReadStream(track, {start: 0, end: 9999});
+  stream.on('data', function (data) {
+    processDataOnce(data, callback);
   });
 }
 
