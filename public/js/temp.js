ed3b55ce67e9244189a444d8645aa6827de72771
Jon Milner <email@jonmilner.com>
2014-05-30 15:04:56 -0400

    moved view links to sidebar, removed main toolbar, removed grid view, added list view for albums, added view sidebar header with sort drop down and search field, additional minor ui changes

--- public/js/temp.js
+++ public/js/temp.js
@@ -1,17 +1,3 @@
-// Google Web Fonts
-WebFontConfig = {
-  google: { families: [ 'Cabin+Condensed:400,700:latin' ] }
-};
-(function() {
-  var wf = document.createElement('script');
-  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-  wf.type = 'text/javascript';
-  wf.async = 'true';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(wf, s);
-})();
-
 // Toggle Now Playing Artwork
 var resizeArtwork = $('.resize-artwork');
     nowPlayingArtworkLarge = $('.now-playing-artwork-large');
